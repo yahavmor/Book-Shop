2e4672ef07283871c41c1c307b8452214288c665
yahav mor <yahavmor77@gmail.com>
2025-06-23 22:08:21 +0300

    initialze hard coded table

---
+++ js/book.controller.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function onInit(){
+    render()
+}
+
+// function render(){
+//     const books = getBooks();
+//     const elBooks = document.querySelectorAll('.book');
+//     console.log(elBooks);
+//     var strHTMLs = '';
+    
+//     elBooks.innerHTML = strHTMLs;
+// }
---
+++ js/book.service.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function getBooks(){
+    return [
+        {id: 'bj5930', title: '1984', price: 14,imgUrl:'https://example.com/1984.jpg'},
+        {id: 'bj5931', title: 'To Kill a Mockingbird', price: 12,imgUrl:'https://example.com/to-kill-a-mockingbird.jpg'},
+        {id: 'bj5932', title: 'The Great Gatsby', price: 10,imgUrl:'https://example.com/the-great-gatsby.jpg'},
+    ]
+}
